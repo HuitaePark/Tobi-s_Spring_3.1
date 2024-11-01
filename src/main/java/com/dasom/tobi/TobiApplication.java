b370fbea6c839efee2d8cf80b546bb9e07562a72
HuitaePark <qkrgmlxo3174@gmail.com>
2024-11-01 16:20:32 +0900

    테스트 코드 추가

--- src/main/java/com/dasom/tobi/TobiApplication.java
+++ src/main/java/com/dasom/tobi/TobiApplication.java
@@ -1,13 +1,34 @@
 package com.dasom.tobi;
 
+import com.dasom.tobi.dao.UserDao;
+import com.dasom.tobi.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class TobiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         SpringApplication.run(TobiApplication.class, args);
+
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("mmmzzi");
+        user.setName("명지");
+        user.setPassword("1234");
+
+        dao.add(user);
+
+        System.out.println(user.getId()+"등록성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId()+"조회성공");
+
     }
 
 }
--- src/main/java/com/dasom/tobi/dao/UserDao.java
+++ src/main/java/com/dasom/tobi/dao/UserDao.java
@@ -7,8 +7,7 @@ import java.sql.*;
 
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/3306", "root", "26323174");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi_db", "root", "26323174");
 
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
         ps.setString(1, user.getId());
@@ -22,8 +21,7 @@ public class UserDao {
 
     }
     public User get(String id) throws ClassNotFoundException,SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/3306", "root", "26323174");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobi_db", "root", "26323174");
 
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
