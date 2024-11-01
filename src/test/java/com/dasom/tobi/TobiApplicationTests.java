d8b67e6fb34429634212ee25286f4d7787a4e5e0
HuitaePark <qkrgmlxo3174@gmail.com>
2024-11-01 15:58:46 +0900

    초난감 DAO 완성

---
+++ src/test/java/com/dasom/tobi/TobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasom.tobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
