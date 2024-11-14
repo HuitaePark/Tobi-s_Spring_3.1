e2f553e16a6d00f01b7ee02b6eb94c439a819205
HuitaePark <qkrgmlxo3174@gmail.com>
2024-11-14 18:20:04 +0900

    전략패턴 분리

--- src/main/java/com/dasom/tobi/dao/UserDaoDeleteAll.java
+++ src/main/java/com/dasom/tobi/dao/UserDaoDeleteAll.java
@@ -4,9 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class UserDaoDeleteAll extends UserDao{
+public class UserDaoDeleteAll{
 
-    @Override
     protected PreparedStatement makeStatement(Connection c) throws SQLException {
         PreparedStatement preparedStatement = c.prepareStatement("delete from users");
         return  preparedStatement;
